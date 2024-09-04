8831eb4fbe293aad8d19a1e87c1b0850bb65e73b
Luigi-Gobelins <louis.yanne@edu.gobelins.fr>
2024-09-04 17:59:34 +0200

    Ossature 1
    - barrières
    - condition 4
    - mouvement physique

--- Phare/Assets/Scripts/GateOpener.cs
+++ Phare/Assets/Scripts/GateOpener.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GateOpener : MonoBehaviour
 {
+    private int currentLevel;
+
+    [SerializeField] private DisplayManager displayManager;
+
+    [SerializeField] private GameObject controlledBall;
+    private Rigidbody rb;
+
+    [SerializeField] private GameObject level2Barrier;
+    [SerializeField] private GameObject level3Barrier;
+    [SerializeField] private GameObject level4Barrier;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentLevel = 0;
+        rb = controlledBall.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        //Condition de passage au niveau 2
+
+        //Condition de passage au niveau 3
+
+        //Condition de passage au niveau 4
+        if (rb.velocity.magnitude >= 60 && currentLevel == 3)
+        {
+            currentLevel++;
+            Destroy(level3Barrier);
+            displayManager.DisplayProcess(currentLevel);
+        }
     }
 }
